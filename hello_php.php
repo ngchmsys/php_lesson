587a1deabfc2da3eadb6eee370a4ddd03b3e687d
ngchmsys <ngchmsys@gmail.com>
2021-01-21 13:21:09 +0900

    Hello PHP

---
+++ hello_php.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ja">
+    <head>
+        <meta charset="utf-8">
+        <title>Hello PHP</title>
+    </head>
+    <body>
+        <h1><?php echo 'Hello PHP'; ?></h1>
+    </body>
+</html>
